c029844cdadcdbe96bfa7f9a96c063a7de73abbe
LakshmiTeja17 <jlakshmiteja17@gmail.com>
2020-06-10 12:16:06 +0530

    init

--- al2bi.py
+++ al2bi.py
@@ -1,145 +1,198 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from sklearn.datasets import load_iris
-from sklearn.neighbors import KNeighborsClassifier
-from modAL.models import ActiveLearner
+from modAL.models import ActiveLearner, Committee
+from modAL.disagreement import vote_entropy_sampling, max_disagreement_sampling
 from sklearn.ensemble import RandomForestClassifier
 from modAL.uncertainty import uncertainty_sampling, margin_sampling, entropy_sampling
-
 from sklearn.datasets import load_digits
+from sklearn.datasets import load_iris
 
-import numpy as np
+
+
+def get_learner(query_strategy):
+
+  global X_train, y_train
+
+  # Specify our core estimator along with it's active learning model.
+  learner = ActiveLearner(estimator=RandomForestClassifier(),
+                          query_strategy=query_strategy,
+                          X_training=X_train, y_training=y_train)
+
+  return learner
+
+
+def get_committee(query_strategy):
+
+  global X_train, y_train
+
+  # initializing Committee members
+  n_members = 5
+  learner_list = list()
+
+  for member_idx in range(n_members):
+      # initializing learner
+      learner = ActiveLearner(
+          estimator=RandomForestClassifier(),
+          X_training=X_train, y_training=y_train
+      )
+      learner_list.append(learner)
+
+  # assembling the committee
+  committee = Committee(learner_list=learner_list,
+                          query_strategy=vote_entropy_sampling)
+
+  return committee
+
+
+def plot_predictions(learner, percent, query_strategy):
+
+  global X_raw, y_raw
+
+  # Isolate the data we'll need for plotting.
+  predictions = learner.predict(X_raw)
+  is_correct = (predictions == y_raw)
+
+  # Plot our classification results.
+  fig, ax = plt.subplots(figsize=(8.5, 6), dpi=130)
+  ax.scatter(x=x_component[is_correct],  y=y_component[is_correct],  c='g', marker='+', label='Correct',   alpha=8/10)
+  ax.scatter(x=x_component[~is_correct], y=y_component[~is_correct], c='r', marker='x', label='Incorrect', alpha=8/10)
+  ax.legend(loc='lower right')
+  ax.set_title(str(percent * 100) + " percent labelled " + query_strategy + " class predictions (Accuracy: {score:.3f})".format(score= (is_correct.sum()/ len(y_raw))))
+  plt.show()
+
+
+def sampling(learner, num_queries, performance):
+
+  global X_pool, y_pool, X_raw, y_raw
+
+  # Allow our model to query our unlabeled dataset for the most
+  # informative points according to our query strategy (uncertainty sampling).
+  for index in range(num_queries):
+
+    query_index, query_instance = learner.query(X_pool)
+
+    # Teach our ActiveLearner model the record it has requested.
+    X, y = X_pool[query_index].reshape(1, -1), y_pool[query_index].reshape(1, )
+    learner.teach(X=X, y=y)
+
+    # Remove the queried instance from the unlabeled pool.
+    X_pool, y_pool = np.delete(X_pool, query_index, axis=0), np.delete(y_pool, query_index)
+
+    # Calculate and report our model's accuracy.
+    model_accuracy = learner.score(X_raw, y_raw)
+    print('Accuracy after query {n}: {acc:0.4f}'.format(n=index + 1, acc=model_accuracy))
+
+    # Save our model's performance for plotting.
+    performance.append(model_accuracy)
+
+  return performance
+
+
+def plot_accuracies(performance_history, fig, ax, label ):
+
+  ax.plot(performance_history, label = label)
+  ax.scatter(range(len(performance_history)), performance_history, s=13)
+
+  ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(nbins=5, integer=True))
+  ax.yaxis.set_major_locator(mpl.ticker.MaxNLocator(nbins=10))
+  ax.yaxis.set_major_formatter(mpl.ticker.PercentFormatter(xmax=1))
+
+  ax.set_ylim(bottom=min(performance_history), top=1)
+  ax.grid(True)
+
+  ax.set_title('Iterative Accuracy')
+  ax.set_xlabel('Query Iteration')
+  ax.set_ylabel('Classification Accuracy')
+
+def main(query_strategy, str_query_strategy, is_committee):
+
+  global X_pool, y_pool, training_indices
+  # Isolate the non-training examples we'll be querying.
+  X_pool = np.delete(X_raw, training_indices, axis=0)
+  y_pool = np.delete(y_raw, training_indices, axis=0)
+
+  if is_committee == False:
+    learner = get_learner(query_strategy)
+  else:
+    learner = get_committee(query_strategy)
+
+  plot_predictions(learner, 0.1, str_query_strategy)
+
+  # Record our learner's score on the raw data.
+  unqueried_score = learner.score(X_raw, y_raw)
+  performance = [unqueried_score]
+
+  performance = sampling(learner, int(4 * len(X_raw) / 10), performance)
+
+  plot_predictions(learner, 0.4, str_query_strategy)
+  return performance
+
+
+
 
 # Set our RNG seed for reproducibility.
 RANDOM_STATE_SEED = 123
 np.random.seed(RANDOM_STATE_SEED)
 
-iris = load_digits()
-X_raw = iris['data']
-y_raw = iris['target']
-
-from sklearn.decomposition import PCA
+dataset = load_iris()
+X_raw = dataset['data']
+y_raw = dataset['target']
 
 # -------------for visualization---------------
 # # Define our PCA transformer and fit it onto our raw dataset.
-# pca = PCA(n_components=2, random_state=RANDOM_STATE_SEED)
-# transformed_iris = pca.fit_transform(X=X_raw)
-# transformed_iris = X_raw
-transformed_iris = X_raw
+pca = PCA(n_components=2, random_state=RANDOM_STATE_SEED)
+transformed_dataset = pca.fit_transform(X=X_raw)
 
-
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
-'''
 # Isolate the data we'll need for plotting.
-x_component, y_component = transformed_iris[:, 0], transformed_iris[:, 1]
+x_component, y_component = transformed_dataset[:, 0], transformed_dataset[:, 1]
 
 # Plot our dimensionality-reduced (via PCA) dataset.
 plt.figure(figsize=(8.5, 6), dpi=130)
 plt.scatter(x=x_component, y=y_component, c=y_raw, cmap='viridis', s=50, alpha=8/10)
-plt.title('Iris classes after PCA transformation')
+plt.title('Dataset classes after PCA transformation')
 plt.show()
-'''
-  
+
+
 # Isolate our examples for our labeled dataset.
 percent = 0.1
-n_labeled_examples = X_raw.shape[0] # len
-training_indices = np.random.randint(low=0, high=n_labeled_examples + 1, size=int(n_labeled_examples * percent))
+n_labeled_examples = X_raw.shape[0]
+training_indices = np.random.randint(low=0, high=n_labeled_examples, size=int(n_labeled_examples * percent))
 
 X_train = X_raw[training_indices]
 y_train = y_raw[training_indices]
+X_pool = None
+y_pool = None
 
-# Isolate the non-training examples we'll be querying.
-X_pool = np.delete(X_raw, training_indices, axis=0)
-y_pool = np.delete(y_raw, training_indices, axis=0)
+print()
+choice = input("Select a choice:\n\n1) Uncertainty Sampling\n2) QBC Sampling\n3) Version Space Sampling\n4) Random Sampling\n5) Cluster based sampling\n\n")
+choice = int(choice)
 
+if (choice == 1):
 
+  performance1 = main(uncertainty_sampling, "Uncertainty Sampling", False)
+  performance2 = main(margin_sampling, "Margin Sampling", False)
+  performance3 = main(entropy_sampling, "Entropy Sampling", False)
 
-from sklearn.neighbors import KNeighborsClassifier
-from modAL.models import ActiveLearner
+  fig = plt.figure()
+  ax = plt.subplot(111)
+  plot_accuracies(performance1, fig, ax, "Uncertainty Sampling")
+  plot_accuracies(performance2, fig, ax, "Margin Sampling")
+  plot_accuracies(performance3, fig, ax, "Entropy Sampling")
+  ax.legend()
+  plt.show()
+  fig.savefig("Accuracies_Uncertainty.png")
 
-# Specify our core estimator along with it's active learning model.
-knn = KNeighborsClassifier(n_neighbors=3)
-learner = ActiveLearner(estimator=RandomForestClassifier(),
-                        query_strategy=uncertainty_sampling,
-                        X_training=X_train, y_training=y_train)
+elif (choice == 2):
 
+  performance1 = main(uncertainty_sampling, "Vote Entropy Sampling", True)
+  performance2 = main(margin_sampling, "KL Divergence Sampling", True)
 
-# Isolate the data we'll need for plotting.
-predictions = learner.predict(X_raw)
-is_correct = (predictions == y_raw)
-
-
-# Record our learner's score on the raw data.
-unqueried_score = learner.score(X_raw, y_raw)
-
-# Plot our classification results.
-'''
-fig, ax = plt.subplots(figsize=(8.5, 6), dpi=130)
-ax.scatter(x=x_component[is_correct],  y=y_component[is_correct],  c='g', marker='+', label='Correct',   alpha=8/10)
-ax.scatter(x=x_component[~is_correct], y=y_component[~is_correct], c='r', marker='x', label='Incorrect', alpha=8/10)
-ax.legend(loc='lower right')
-ax.set_title("ActiveLearner class predictions (Accuracy: {score:.3f})".format(score=unqueried_score))
-plt.show()
-'''
-
-N_QUERIES = 20
-performance_history = [unqueried_score]
-
-# Allow our model to query our unlabeled dataset for the most
-# informative points according to our query strategy (uncertainty sampling).
-for index in range(N_QUERIES):
-  query_index, query_instance = learner.query(X_pool)
-
-  # Teach our ActiveLearner model the record it has requested.
-  X, y = X_pool[query_index].reshape(1, -1), y_pool[query_index].reshape(1, )
-  learner.teach(X=X, y=y)
-
-  # Remove the queried instance from the unlabeled pool.
-  X_pool, y_pool = np.delete(X_pool, query_index, axis=0), np.delete(y_pool, query_index)
-
-  # Calculate and report our model's accuracy.
-  model_accuracy = learner.score(X_raw, y_raw)
-  print('Accuracy after query {n}: {acc:0.4f}'.format(n=index + 1, acc=model_accuracy))
-
-  # Save our model's performance for plotting.
-  performance_history.append(model_accuracy)
-
-
-# Plot our performance over time.
-fig, ax = plt.subplots(figsize=(8.5, 6), dpi=130)
-
-ax.plot(performance_history)
-ax.scatter(range(len(performance_history)), performance_history, s=13)
-
-ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(nbins=5, integer=True))
-ax.yaxis.set_major_locator(mpl.ticker.MaxNLocator(nbins=10))
-ax.yaxis.set_major_formatter(mpl.ticker.PercentFormatter(xmax=1))
-
-ax.set_ylim(bottom=0, top=1)
-ax.grid(True)
-
-ax.set_title('Incremental classification accuracy')
-ax.set_xlabel('Query iteration')
-ax.set_ylabel('Classification Accuracy')
-
-plt.show()
-
-# Isolate the data we'll need for plotting. 
-predictions = learner.predict(X_raw)
-is_correct = (predictions == y_raw)
-
-'''
-# Plot our updated classification results once we've trained our learner.
-fig, ax = plt.subplots(figsize=(8.5, 6), dpi=130)
-
-ax.scatter(x=x_component[is_correct],  y=y_component[is_correct],  c='g', marker='+', label='Correct',   alpha=8/10)
-ax.scatter(x=x_component[~is_correct], y=y_component[~is_correct], c='r', marker='x', label='Incorrect', alpha=8/10)
-
-ax.set_title('Classification accuracy after {n} queries: {final_acc:.3f}'.format(n=N_QUERIES, final_acc=performance_history[-1]))
-ax.legend(loc='lower right')
-
-plt.show()
-'''
+  fig = plt.figure()
+  ax = plt.subplot(111)
+  plot_accuracies(performance1, fig, ax, "Vote Entropy Sampling")
+  plot_accuracies(performance2, fig, ax, "KL Divergence Sampling")
+  ax.legend()
+  plt.show()
+  fig.savefig("Accuracies_QBC.png")
--- al2bii.py
+++ al2bii.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Set our RNG seed for reproducibility.
-RANDOM_STATE_SEED = 1
+RANDOM_STATE_SEED = 123
 np.random.seed(RANDOM_STATE_SEED)
 
 
@@ -14,20 +14,19 @@ from sklearn.datasets import load_iris
 iris = load_iris()
 
 # visualizing the classes
-'''
 with plt.style.context('seaborn-white'):
     plt.figure(figsize=(7, 7))
     pca = PCA(n_components=2).fit_transform(iris['data'])
     plt.scatter(x=pca[:, 0], y=pca[:, 1], c=iris['target'], cmap='viridis', s=50)
     plt.title('The iris dataset')
     plt.show()
-'''
 
 from copy import deepcopy
 
 # generate the pool
 X_pool = deepcopy(iris['data'])
 y_pool = deepcopy(iris['target'])
+n_initial = int(0.1 * len(X_pool))
 
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
@@ -40,8 +39,9 @@ learner_list = list()
 
 for member_idx in range(n_members):
     # initial training data
-    n_initial = 2
-    train_idx = np.random.choice(range(X_pool.shape[0]), size=n_initial, replace=False)
+    percent = 0.1
+    n_labeled_examples = X_pool.shape[0]
+    train_idx = np.random.randint(low=0, high=n_labeled_examples, size=int(n_labeled_examples * percent))
     X_train = X_pool[train_idx]
     y_train = y_pool[train_idx]
 
@@ -58,9 +58,8 @@ for member_idx in range(n_members):
 
 # assembling the committee
 committee = Committee(learner_list=learner_list,
-                        query_strategy=vote_entropy_sampling)
+                        query_strategy=max_disagreement_sampling)
 
-'''
 with plt.style.context('seaborn-white'):
     plt.figure(figsize=(n_members*7, 7))
     for learner_idx, learner in enumerate(committee):
@@ -68,23 +67,20 @@ with plt.style.context('seaborn-white'):
         plt.scatter(x=pca[:, 0], y=pca[:, 1], c=learner.predict(iris['data']), cmap='viridis', s=50)
         plt.title('Learner no. %d initial predictions' % (learner_idx + 1))
     plt.show()
-'''
 
 unqueried_score = committee.score(iris['data'], iris['target'])
 
-'''
 with plt.style.context('seaborn-white'):
     plt.figure(figsize=(7, 7))
     prediction = committee.predict(iris['data'])
     plt.scatter(x=pca[:, 0], y=pca[:, 1], c=prediction, cmap='viridis', s=50)
     plt.title('Committee initial predictions, accuracy = %1.3f' % unqueried_score)
     plt.show()
-'''
 
 performance_history = [unqueried_score]
 
 # query by committee
-n_queries = 20
+n_queries = 4 * n_initial
 for idx in range(n_queries):
     query_idx, query_instance = committee.query(X_pool)
     committee.teach(
@@ -98,7 +94,6 @@ for idx in range(n_queries):
     print('Accuracy after query {n}: {acc:0.4f}'.format(n=idx + 1, acc=committee.score(iris['data'], iris['target'])))
 
 
-'''
 # visualizing the final predictions per learner
 with plt.style.context('seaborn-white'):
     plt.figure(figsize=(n_members*7, 7))
@@ -117,7 +112,7 @@ with plt.style.context('seaborn-white'):
     plt.title('Committee predictions after %d queries, accuracy = %1.3f'
               % (n_queries, committee.score(iris['data'], iris['target'])))
     plt.show()
-'''
+
 # Plot our performance over time.
 fig, ax = plt.subplots(figsize=(8.5, 6), dpi=130)
 
